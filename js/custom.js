4feec770025fffba4061dbb75e9cf538b1360e71
Jukka Koskinen <jukka@aerobit.fi>
2022-04-13 07:33:58 +0300

    Unify styles and navigation between use cases and other pages

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,47 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-  if (currentScrollPos < 30) {
-    $(".navbar").css("background-color", "transparent");
-  } else {
-    $(".navbar").css("background-color", "#202529");
+(function () {
+  var $mainNav = $(".main-nav");
+
+  window.addEventListener("load", function () {
+    initDropdowns();
+    initStickyNav();
+  });
+
+  window.addEventListener("scroll", function () {
+    updateStickyNav();
+  });
+
+  $("#navbar-collapse").on("show.bs.collapse", function () {
+    $mainNav.addClass("is-expanded");
+  });
+
+  $("#navbar-collapse").on("hidden.bs.collapse", function () {
+    $mainNav.removeClass("is-expanded");
+  });
+
+  function initDropdowns() {
+    if (window.location.pathname === "/") {
+      return;
+    }
+    $mainNav.find(".dropdown-item").each(function (_index, element) {
+      var $dropdown = $(element).closest(".dropdown-menu");
+      var $element = $(element);
+
+      if ($element.attr("href").match(window.location.pathname)) {
+        $element.addClass("active");
+        if ($(window).width() < 768) {
+          $dropdown.addClass("show");
+        }
+      }
+    });
   }
-};
+
+  function initStickyNav() {
+    $mainNav.css({ transition: "none" });
+    updateStickyNav();
+    setTimeout(() => $mainNav.css({ transition: "" }));
+  }
+
+  function updateStickyNav() {
+    $mainNav.toggleClass("is-sticky", window.pageYOffset >= 30);
+  }
+})();
